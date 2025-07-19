9800b0ce14e5e991644d931c6c89cb705b1fe9cb
may-leth <mayleth9609@gmail.com>
2025-07-19 16:48:31 +0200

    Initial commit: Project base structure, Maven files and .gitignore

---
+++ src/main/java/com/may/Ninja/Mission/Manager/NinjaMissionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.may.Ninja.Mission.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaMissionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaMissionManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/may/Ninja/Mission/Manager/NinjaMissionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.may.Ninja.Mission.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaMissionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
