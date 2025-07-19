8391fad9ae17e50088c732a9874e8f16badf37cf
may-leth <mayleth9609@gmail.com>
2025-07-19 19:01:57 +0200

    test: implement integration tests for GET/missions and GET/missions/{id}; modify MissionService to handle mission not found with 404 error

--- src/main/java/com/may/Ninja/Mission/Manager/services/MissionService.java
+++ src/main/java/com/may/Ninja/Mission/Manager/services/MissionService.java
@@ -5,7 +5,9 @@ import com.may.Ninja.Mission.Manager.dtos.MissionResponse;
 import com.may.Ninja.Mission.Manager.models.Mission;
 import com.may.Ninja.Mission.Manager.repositories.MissionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -24,7 +26,7 @@ public class MissionService {
 
     public MissionResponse getMissionById(Long id){
         Mission mission = missionRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Mission not found with id " + id));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Mission not found with id " + id));
         return MissionMapper.entityToDto(mission);
     }
 }
