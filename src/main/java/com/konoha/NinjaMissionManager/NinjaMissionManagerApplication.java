b546ac486a293c8a893db25ce888ddcd75bdba1a
may-leth <mayleth9609@gmail.com>
2025-08-06 08:38:34 +0200

    feat: project structure

---
+++ src/main/java/com/konoha/NinjaMissionManager/NinjaMissionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.konoha.NinjaMissionManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaMissionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaMissionManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konoha/NinjaMissionManager/NinjaMissionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konoha.NinjaMissionManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaMissionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
