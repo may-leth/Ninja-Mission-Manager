8f917667e3d5ccc1d8c15ae5621e8d4a9e16b411
may-leth <mayleth9609@gmail.com>
2025-09-02 14:44:25 +0200

    feat: add exception bad credentials to global exception handler

--- src/main/java/com/konoha/NinjaMissionManager/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/konoha/NinjaMissionManager/exceptions/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -75,5 +76,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .path(request.getDescription(false).substring(4))
                 .build();
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException exception, WebRequest request){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message("Invalid email or password.")
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST)
+                .path(request.getDescription(false).substring(4))
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
